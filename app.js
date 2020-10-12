25e67b6715b6e42f1bcd2eeb6771601fe7fef060
Ngonidzashe Mangudya <38191932+iamngoni@users.noreply.github.com>
2020-10-12 12:23:05 +0200

    changed game urls

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var panesApp = new Vue({
     this.username += prompt("Choose your username")
     $.ajax({
       type: 'GET',
-      url: 'https://iamngoni-env.eba-webtmyzh.us-east-2.elasticbeanstalk.com/memory/get'
+      url: 'https://www.iamngoni.co.zw/memory/get'
     }).done((response) => {
       this.halloffame = response
     }).fail((response) => {
@@ -128,7 +128,7 @@ var panesApp = new Vue({
         this.hasEnded = true
         $.ajax({
           type: 'POST',
-          url: "https://iamngoni-env.eba-webtmyzh.us-east-2.elasticbeanstalk.com/memory/save",
+          url: "https://www.iamngoni.co.zw/memory/save",
           data: {username: this.username, score: this.overalScore}
         }).done((response) => {
           this.halloffame = response
